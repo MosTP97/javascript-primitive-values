ec4fec5b36bf6ce709ea6f2135f9ceb39b064eff
Mos <theeraphat.sukn@gmail.com>
2024-08-16 01:26:20 +0700

    commit Assignment

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPrice = 27000;
+let Discount = totalPrice*25/100;
+let amount = totalPrice-Discount;
--- ex-6.js
+++ ex-6.js
@@ -1,6 +1,11 @@
 // Exercise #6: Displaying message
 
 let totalParts = 13000;
+let total = "Total";
+let machineParts ="Machine parts";
 
-let concatSentence;
-let literalSentence;
+let concatSentence = total+": "+totalParts+" "+machineParts;
+let literalSentence = `${total}: ${totalParts} ${machineParts}`;
+
+console.log(concatSentence);
+console.log(literalSentence);
